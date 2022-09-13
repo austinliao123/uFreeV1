195ca8b822a854fdc15904320874003a2efc3e13
austinliao123 <austinliao123@hotmail.com>
2022-09-12 21:50:42 -0400

    login and register mostly done

---
+++ uFreeV1/Controllers/FeedViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  uFreeV1
+//
+//  Created by Austin  Liao on 9/5/22.
+//
+
+import UIKit
+import FirebaseAuth
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        validateAuth()
+        
+    }
+    
+    private func validateAuth() {
+        if FirebaseAuth.Auth.auth().currentUser == nil {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+
+}
+
